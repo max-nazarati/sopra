305d2a786612aca848ee10e8ed4ab6aa9379053f
Zacharias Häringer <Zacharias.Haeringer@gmx.de>
2019-05-19 11:27:18 +0200

    added stuff like double click and MANY other

--- src/KernelPanic/InputManager.cs
+++ src/KernelPanic/InputManager.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
 
 namespace KernelPanic
@@ -12,38 +8,110 @@ namespace KernelPanic
     /// </summary>
     public sealed class InputManager
     {
-        private KeyboardState mCurrentKeyboardState;
-        private KeyboardState mPreviousKeyboardState;
+        private KeyboardState mCurrentKeyboardState, mPreviousKeyboardState;
+        private MouseState mCurrentMouseState, mPreviousMouseState;
         private static InputManager sInstance;
+        private Tuple<int, int> mLatestMouseLeftClickPosition = new Tuple<int, int>(-1, -1);
+        private Tuple<int, int> mLatestMouseMiddleClickPosition = new Tuple<int, int>(-1, -1);
+        private Tuple<int, int> mLatestMouseRightClickPosition = new Tuple<int, int>(-1, -1);
+        private const int MaximumDoubleClickDelay = 20; // You have 30 frames to enter your double click (1/2 sec at 60 FPS)
+        private int mDoubleClickFrameCount = 0; // Left MouseButton
+        private bool mRecentDoubleClicked;
 
-        public static InputManager Default
+        /// <summary>
+        /// Left, Middle and Right MouseButton
+        /// </summary>
+        public enum MouseButton : byte
         {
-            get
-            {
-                if (sInstance == null)
-                {
-                    sInstance = new InputManager();
-                }
+            Left, Middle, Right
+        }
 
-                return sInstance;
+        public static InputManager Default => sInstance ?? (sInstance = new InputManager());
+
+        /// <summary>
+        /// updates the Input states, should be called in the main update function.
+        /// </summary>
+        public void Update()
+        {
+            // updating the keyboard
+            mPreviousKeyboardState = mCurrentKeyboardState;
+            mCurrentKeyboardState = Keyboard.GetState();
+
+            // updating the mouse
+            mPreviousMouseState = mCurrentMouseState;
+            mCurrentMouseState = Mouse.GetState();
+
+            DoubleClickUpdate();
+
+            
+            UpdateMouseClickPosition();
+            
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        private void DoubleClickUpdate()
+        {
+            // update the timing variable
+            if (mDoubleClickFrameCount > 0)
+            {
+                mDoubleClickFrameCount -= 1;
+            }
+
+            // check for Input to reset timer or return successful double click
+            if (MousePressed(MouseButton.Left))
+            {
+                if (mDoubleClickFrameCount > 0)
+                {
+                    mDoubleClickFrameCount = 0;
+                    mRecentDoubleClicked = true;
+                    return;
+                }
+                mDoubleClickFrameCount = MaximumDoubleClickDelay;
+                mRecentDoubleClicked = false;
+            }
+            else
+            {
+                mRecentDoubleClicked = false;
+            }
+                
+
+
+
+        }
+
+        public bool DoubleClick()
+        {
+            return mRecentDoubleClicked;
+        }
+
+        /// <summary>
+        /// Checking for and updating the newest mouse Click positions
+        /// </summary>
+        private void UpdateMouseClickPosition()
+        {
+            if (MousePressed(MouseButton.Left))
+            {
+                mLatestMouseLeftClickPosition = new Tuple<int, int>(mCurrentMouseState.X, mCurrentMouseState.Y);
+            }
+            if (MousePressed(MouseButton.Middle))
+            {
+                mLatestMouseMiddleClickPosition = new Tuple<int, int>(mCurrentMouseState.X, mCurrentMouseState.Y);
+            }
+            if (MousePressed(MouseButton.Right))
+            {
+                mLatestMouseRightClickPosition = new Tuple<int, int>(mCurrentMouseState.X, mCurrentMouseState.Y);
             }
         }
 
-        /// <summary>
-        /// updates the Key states, should be used frequently in the main.
-        /// </summary>
-        internal void Update()
-        {
-            mPreviousKeyboardState = mCurrentKeyboardState;
-            mCurrentKeyboardState = Keyboard.GetState();
-        }
 
         /// <summary>
-        /// checks if a list of Keyboard Buttons have been pressed (at this exact moment)
+        /// checks if any of the Keyboard Buttons has been pressed (at this exact moment)
         /// </summary>
         /// <param name="keys">Keys that should be checked</param>
         /// <returns>True if any of the keys was pressed</returns>
-        private bool KeyPressed(params Keys[] keys)
+        public bool KeyPressed(params Keys[] keys)
         {
             foreach (Keys key in keys)
             {
@@ -57,11 +125,11 @@ namespace KernelPanic
         }
 
         /// <summary>
-        /// checks if a list of Keyboard Buttons have been released (at this exact moment)
+        /// checks if any of the Keyboard Buttons has been released (at this exact moment)
         /// </summary>
         /// <param name="keys">Keys that should be checked</param>
         /// <returns>True if any of the keys was released</returns>
-        private bool KeyReleased(params Keys[] keys)
+        public bool KeyReleased(params Keys[] keys)
         {
             foreach (Keys key in keys)
             {
@@ -75,11 +143,11 @@ namespace KernelPanic
         }
 
         /// <summary>
-        /// checks if a list of Keyboard Buttons are currently being pressed
+        /// checks if any of the Keyboard Buttons is currently being pressed
         /// </summary>
         /// <param name="keys">Keys that should be checked</param>
         /// <returns>True if any of the keys was down</returns>
-        private bool KeyDown(params Keys[] keys)
+        public bool KeyDown(params Keys[] keys)
         {
             foreach (Keys key in keys)
             {
@@ -93,13 +161,182 @@ namespace KernelPanic
         }
 
         /// <summary>
-        /// checks if a list of Keyboard Buttons are currently not being pressed
+        /// checks if any of the Keyboard Buttons is currently not being pressed
+        /// (this is not equal to '!KeyDown(keys)'
         /// </summary>
         /// <param name="keys">Keys that should be checked</param>
         /// <returns>True if any of the keys was up</returns>
-        private bool KeyUp(params Keys[] keys)
+        public bool KeyUp(params Keys[] keys)
         {
-            return !KeyDown(keys);
+            foreach (Keys key in keys)
+            {
+                if (mCurrentKeyboardState.IsKeyUp(key))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// calculates the X and Y difference since the last update
+        /// </summary>
+        /// <returns>Tuple with X and Y distance</returns>
+        public Tuple<int, int> MouseMovementTuple()
+        {
+            return new Tuple<int, int> (mCurrentMouseState.X - mPreviousMouseState.X, mCurrentMouseState.Y - mPreviousMouseState.Y);
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="mouseButtons">enum object: Left, Middle, Right</param>
+        /// <returns>true if any of the buttons is freshly pressed</returns>
+        private bool MousePressed(params MouseButton[] mouseButtons)
+        {
+            bool pressed = false;
+            foreach (var mouseButton in mouseButtons)
+            {
+                switch (mouseButton)
+                {
+                    case MouseButton.Left:
+                        pressed |= mCurrentMouseState.LeftButton == ButtonState.Pressed &&
+                                   mPreviousMouseState.LeftButton != ButtonState.Pressed;
+                        break;
+                    case MouseButton.Middle:
+                        pressed |= mCurrentMouseState.MiddleButton == ButtonState.Pressed &&
+                                   mPreviousMouseState.MiddleButton != ButtonState.Pressed;
+                        break;
+                    case MouseButton.Right:
+                        pressed |= mCurrentMouseState.RightButton == ButtonState.Pressed &&
+                                   mPreviousMouseState.RightButton != ButtonState.Pressed;
+                        break;
+                    // default:
+                    //    throw new ArgumentOutOfRangeException("nameof(mouseButtons)", "I can only check for the MouseButtons listed in the Enum");
+                }
+            }
+
+            return pressed;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="mouseButtons"></param>
+        /// <returns></returns>
+        public bool MouseReleased(params MouseButton[] mouseButtons)
+        {
+            bool released = false;
+            foreach (var mouseButton in mouseButtons)
+            {
+                switch (mouseButton)
+                {
+                    case MouseButton.Left:
+                        released |= mCurrentMouseState.LeftButton != ButtonState.Pressed &&
+                                    mPreviousMouseState.LeftButton == ButtonState.Pressed;
+                        break;
+                    case MouseButton.Middle:
+                        released |= mCurrentMouseState.MiddleButton != ButtonState.Pressed &&
+                                    mPreviousMouseState.MiddleButton == ButtonState.Pressed;
+                        break;
+                    case MouseButton.Right:
+                        released |= mCurrentMouseState.RightButton != ButtonState.Pressed &&
+                                    mPreviousMouseState.RightButton == ButtonState.Pressed;
+                        break;
+                    // default:
+                    //    throw new ArgumentOutOfRangeException("nameof(mouseButtons)", "I can only check for the MouseButtons listed in the Enum");
+                }
+            }
+
+            return released;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <param name="mouseButtons"></param>
+        /// <returns></returns>
+        public bool MouseDown(params MouseButton[] mouseButtons)
+        {
+            bool down = false;
+            foreach (var mouseButton in mouseButtons)
+            {
+                switch (mouseButton)
+                {
+                    case MouseButton.Left:
+                        down |= mCurrentMouseState.LeftButton == ButtonState.Pressed;
+                        break;
+                    case MouseButton.Middle:
+                        down |= mCurrentMouseState.MiddleButton == ButtonState.Pressed;
+                        break;
+                    case MouseButton.Right:
+                        down |= mCurrentMouseState.RightButton == ButtonState.Pressed;
+                        break;
+                    // default:
+                    //    throw new ArgumentOutOfRangeException("nameof(mouseButtons)", "I can only check for the MouseButtons listed in the Enum");
+                }
+            }
+
+            return down;
+        }
+
+        /// <summary>
+        /// TODO WIP should this even be here or in camera?
+        /// </summary>
+        /// <returns></returns>
+        public bool MouseAtLeftScreenBorder()
+        {
+            return mCurrentMouseState.X < 400 && mCurrentMouseState.X > 0;
+        }
+
+        /// <summary>
+        /// TODO WIP should this even be here or in camera?
+        /// </summary>
+        /// <returns></returns>
+        public bool MouseAtTopScreenBorder()
+        {
+            return mCurrentMouseState.Y < 200 && mCurrentMouseState.Y > 0;
+        }
+
+        /// <summary>
+        /// TODO WIP should this even be here or in camera?
+        /// </summary>
+        /// <returns></returns>
+        public bool MouseAtRightScreenBorder()
+        {
+            return mCurrentMouseState.X < 1520 && mCurrentMouseState.X > 1920;
+        }
+
+        /// <summary>
+        /// TODO WIP should this even be here or in camera?
+        /// </summary>
+        /// <returns></returns>
+        public bool MouseAtBottomScreenBorder()
+        {
+            return mCurrentMouseState.Y < 1080 && mCurrentMouseState.Y > 880;
+        }
+
+        /// <summary>
+        /// calculate how far the scroll wheel got turned
+        /// </summary>
+        /// <returns>negative value for zooming out</returns>
+        public int ScrollWheelMovement()
+        {
+            return mCurrentMouseState.ScrollWheelValue - mPreviousMouseState.ScrollWheelValue;
+        }
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        /// <returns></returns>
+        private Tuple<bool, Tuple<int, int>, Tuple<int, int>> MouseDragged(MouseButton mouseButton)
+        {
+            var startPointXy = new Tuple<int, int> (mCurrentMouseState.X, mCurrentMouseState.Y);
+            var endPointXy = new Tuple<int, int>(mCurrentMouseState.X, mCurrentMouseState.Y);
+
+            var result = new Tuple<bool, Tuple<int, int>, Tuple<int, int>>(false, startPointXy, endPointXy);
+            return result;
         }
     }
 }
