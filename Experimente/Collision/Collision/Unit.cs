f0cd9cf3dfaa99a85dd1a3a478b17d0730f67d41
haeglem <melissaa@tf.uni-freiburg.de>
2019-05-20 21:47:48 +0200

    created an object and collision manager, references #71 #72

---
+++ Experimente/Collision/Collision/Unit.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Collision
+{
+    public sealed class Unit : Entity
+    {
+        /* To select a unit, left-click on it
+         * Further lef-clicks place the unit on different positions
+         * Hold left-mouse button to drag the unit around
+         * Right-clicks make the unit float to a different position
+         * Spacebar to unselect unit
+         */
+
+        private bool mSelected;
+        private Point mMovementGoal = new Point(-1, -1);
+
+        public Unit(int x, int y, int width, int height, Texture2D texture) : base(x, y,
+            width, height, texture)
+        {
+        }
+
+        private bool LeftClick()
+        {
+            if (!mContainerRectangle.Contains(mMouseState.Position)) return false;
+            return mMouseState.LeftButton == ButtonState.Pressed && 
+                   mOldMouseState.LeftButton == ButtonState.Released;
+        }
+
+        private void MoveToClick(Point target, int speed)
+        {
+            Vector2 direction = new Vector2(target.X - (mContainerRectangle.Width / 2) - mContainerRectangle.X,
+                target.Y - (mContainerRectangle.Height / 2) - mContainerRectangle.Y);
+            direction.Normalize();
+            Vector2 normalizedDirection = direction;
+
+            if (normalizedDirection.Length() >= 0.99)
+            {
+                mContainerRectangle.X += (int)(normalizedDirection.X * speed);
+                mContainerRectangle.Y += (int)(normalizedDirection.Y * speed);
+            }
+
+            if (Math.Abs(mContainerRectangle.X + (mContainerRectangle.Width / 2) - target.X) <= 4 &&
+                Math.Abs(mContainerRectangle.Y + (mContainerRectangle.Height / 2) - target.Y) <= 4)
+            {
+                mMovementGoal = new Point(-1, -1);
+            }
+        }
+
+        private void JumpToClick(MouseState mouseState)
+        {
+            mContainerRectangle.Location = new Point(mouseState.X - mContainerRectangle.Width / 2, 
+                mouseState.Y - mContainerRectangle.Height / 2);
+        }
+
+        public void Deselect()
+        {
+            mSelected = false;
+        }
+
+        public new bool Update()
+        {
+            base.Update();
+            if (mSelected)
+            {
+                if (mMouseState.RightButton == ButtonState.Pressed &&
+                    mOldMouseState.RightButton == ButtonState.Released)
+                {
+                    mMovementGoal = mMouseState.Position;
+                    return false;
+                }
+                else if (mMouseState.LeftButton == ButtonState.Pressed)
+                {
+                    JumpToClick(mMouseState);
+                    return false;
+                }
+                else if (mMovementGoal != new Point(-1, -1))
+                {
+                    MoveToClick(mMovementGoal, 10);
+                }
+
+                if (mKeyboardState.IsKeyDown(Keys.Space) &&
+                    mOldKeyboardState.IsKeyUp(Keys.Space))
+                {
+                    mSelected = false;
+                    mMovementGoal = new Point(-1, -1);
+                    return false;
+                }
+            }
+            if (!LeftClick() || mSelected) return false;
+            mSelected = true;
+            return true;
+        }
+    }
+}
