edacaa1d75326320b465aed8ac81147df9a6fcb9
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-05-28 13:52:26 +0200

    Documentation for PurchasableAction

--- src/KernelPanic/PurchasableAction.cs
+++ src/KernelPanic/PurchasableAction.cs
@@ -15,8 +15,19 @@ namespace KernelPanic
             Resource = resource;
         }
         
-        internal virtual bool Available(Player buyer) => ResourceModifier(buyer).get() >= Resource.Price;
+        /// <summary>
+        /// Checks if the given player can afford this action with his resources.
+        /// </summary>
+        /// Resources are either bitcoins or experience points.
+        /// <param name="buyer">The potential buyer.</param>
+        /// <returns>True if <paramref name="buyer"/> can afford the resource.</returns>
+        internal virtual bool Available(Player buyer) => PlayerResourcesLens(buyer).get() >= Resource.Price;
 
+        /// <summary>
+        /// Purchases the action if the given Player can afford it.
+        /// </summary>
+        /// <param name="buyer">The potential buyer.</param>
+        /// <returns>True if <paramref name="buyer"/> bought the resource.</returns>
         internal bool TryPurchase(Player buyer)
         {
             var available = Available(buyer);
@@ -25,14 +36,25 @@ namespace KernelPanic
             return available;
         }
 
+        /// <summary>
+        /// Purchases the action, that is it reduces the <paramref name="buyer"/>'s available resources.
+        /// </summary>
+        /// <param name="buyer">The Player whose available resources are</param>
         protected virtual void Purchase(Player buyer)
         {
-            var resources = ResourceModifier(buyer);
+            var resources = PlayerResourcesLens(buyer);
             resources.set(resources.get() - Resource.Price);
             Purchased?.Invoke(buyer, Resource);
         }
 
-        private (Func<int> get, Action<int> set) ResourceModifier(Player buyer)
+        /// <summary>
+        /// Returns functions (a “lens”) to query and modify the given player's available resources. This uses
+        /// <code>Resource.Currency</code> to query/modify either the buyer's bitcoins or experience points.
+        /// </summary>
+        /// <param name="buyer">The player whose resources will be queried/modified.</param>
+        /// <returns>Functions to query and modify the resources of the given player.</returns>
+        /// <exception cref="InvalidOperationException">throws if <code>Resource.Currency</code> is an unlisted value.</exception>
+        private (Func<int> get, Action<int> set) PlayerResourcesLens(Player buyer)
         {
             switch (Resource.Currency)
             {
@@ -41,7 +63,7 @@ namespace KernelPanic
                 case Currency.Experience:
                     return (() => buyer.ExperiencePoints, i => buyer.ExperiencePoints = i);
                 default:
-                    throw new ArgumentOutOfRangeException();
+                    throw new InvalidOperationException();
             }
         }
     }
