1487e7d7ab621110d45a574bee56dcedf70251a2
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-06-01 18:17:19 +0200

    Documentation for Building.State

--- src/KernelPanic/Entities/Building.cs
+++ src/KernelPanic/Entities/Building.cs
@@ -18,6 +18,27 @@ namespace KernelPanic
 
         public State StateProperty { get; set; }
         
-        public enum State { Active, Inactive, Invalid, Valid };
+        internal enum State
+        {
+            /// <summary>
+            /// The building is able to act, that means it is able to attack enemies.
+            /// </summary>
+            Active,
+            
+            /// <summary>
+            /// The building has been bought and is waiting to become active, that is when no enemies are at its position.
+            /// </summary>
+            Inactive,
+            
+            /// <summary>
+            /// Used during selection of a new place for a building when the current position is not allowed.
+            /// </summary>
+            Invalid,
+            
+            /// <summary>
+            /// Used during selection of a new place for a building when the current position is allowed.
+            /// </summary>
+            Valid
+        };
     }
 }
