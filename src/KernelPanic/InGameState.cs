526d92928749edc8cc0309a981b45f6e8a7d1810
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-06-03 23:38:47 +0200

    Remove unncessary using-directives

--- src/KernelPanic/InGameState.cs
+++ src/KernelPanic/InGameState.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.Serialization;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
