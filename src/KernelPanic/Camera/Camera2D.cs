16009b86425897e365bce4c921c1cbdf4e8400cd
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-06-10 18:22:08 +0200

    [Camera] Fix movement, respect the bounds – zum Zweiten
    Now it also works when zooming

--- src/KernelPanic/Camera/Camera2D.cs
+++ src/KernelPanic/Camera/Camera2D.cs
@@ -19,6 +19,10 @@ namespace KernelPanic
         private Matrix mTransformation;
         private Matrix mInverseTransformation;
 
+        private const float MinZoom = 0.2f;
+        private const float MaxZoom = 2.0f;
+        private const float Movement = 10f;
+
         /// <summary>
         /// Creates a new camera at (0,0) which moves only inside <paramref name="boundingBox"/> with a viewport of size
         /// <paramref name="viewportSize"/>.
@@ -40,28 +44,29 @@ namespace KernelPanic
         /// <inheritdoc />
         public void Update(Point viewportSize, Change x, Change y, Change scrollVertical)
         {
-            if (x.IsNone && y.IsNone && scrollVertical.IsNone)
+            var newZoom = MathHelper.Clamp((float) Math.Pow(1.5, scrollVertical.Direction) * mZoom, MinZoom, MaxZoom);
+            if (Math.Abs(newZoom - mZoom) > 0.1f)
+            {
+                // When zooming we have to recalculate immediately, otherwise
+                // the calculations to stay in the bounding box aren't correct.
+                mZoom = newZoom;
+                RecalculateTransformations(viewportSize);
+            }
+
+            if (x.IsNone && y.IsNone)
                 return;
-                
+
             // Turn the corners of the camera's bounds into screen coordinates.
-            var windowOrigin = Vector2.Transform(Vector2.Zero, InverseTransformation);
-            var upperLeft = Vector2.Transform(mBoundingBox.Location.ToVector2(), InverseTransformation);
-            var lowerRight = Vector2.Transform(
-                (mBoundingBox.Location + mBoundingBox.Size).ToVector2(),
-                InverseTransformation);
-                
+            var windowUpperLeft = Vector2.Transform(Vector2.Zero, InverseTransformation);
+            var windowLowerRight = Vector2.Transform(viewportSize.ToVector2(), InverseTransformation);
+
             // Calculate the ranges in which the movement has to lie.
-            var minDifference = upperLeft - windowOrigin;
-            var maxDifference = lowerRight - windowOrigin - viewportSize.ToVector2();
-            
-            // Turn the ranges back.
-            minDifference = Vector2.Transform(minDifference, Transformation);
-            maxDifference = Vector2.Transform(maxDifference, Transformation);
-           
+            var minDifference = mBoundingBox.Location.ToVector2() - windowUpperLeft;
+            var maxDifference = mBoundingBox.Location.ToVector2() + mBoundingBox.Size.ToVector2() - windowLowerRight;
+
             // Update the camera position.
-            mX += MathHelper.Clamp(x.Direction * 10 / mZoom, minDifference.X, maxDifference.X);
-            mY += MathHelper.Clamp(y.Direction * 10 / mZoom, minDifference.Y, maxDifference.Y);
-            mZoom = MathHelper.Clamp((float) Math.Pow(1.5, scrollVertical.Direction) * mZoom, 0.2f, 2.0f);
+            mX += MathHelper.Clamp(x.Direction * Movement / mZoom, minDifference.X, maxDifference.X);
+            mY += MathHelper.Clamp(y.Direction * Movement / mZoom, minDifference.Y, maxDifference.Y);
 
             // We recalculate the transformation and its inverse after each update, since the update occurs only once
             // during an update-cycle and in each update-cycle both will be used.
