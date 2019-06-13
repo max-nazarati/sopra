e023464a1e36cfc68b4cdeae362a456f1aaa0753
rahnfelj <jrahnfeld@hotmail.de>
2019-06-13 18:46:19 +0200

    Heatmap and Vectorfield implementation for pathplanning

---
+++ src/KernelPanic/BreadthFirstSearch.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace KernelPanic
+{
+    class BreadthFirstSearch
+    {
+        public const int Blocked = -1;
+
+        private bool[,] mExplored;
+        private Heatmap mMap;
+        private Vectorfield mVectorfield;
+        private PriorityQueue mQueue;
+        private Point mGoalPoint;
+
+        public BreadthFirstSearch(Heatmap map, Point goalPoint)
+        {
+            mMap = map;
+            mExplored = new bool[map.Height, map.Width];
+            mVectorfield = new Vectorfield(map.Width, map.Height);
+            mQueue = new PriorityQueue();
+            mGoalPoint = goalPoint;
+        }
+
+        private List<Node> CreateNeighbours(Node node)
+        {
+            var neighbours = new List<Node>();
+            var x = node.Position.X;
+            var y = node.Position.Y;
+            var up = new Point(x, y - 1);
+            var left = new Point(x - 1, y);
+            var down = new Point(x, y + 1);
+            var right = new Point(x + 1, y);
+            var cost = node.Cost + 1; // if we put an higher value as summand we get a 'dumb' search
+
+            if (mMap.IsWalkable(up))
+            {
+                Node nodeUp = new Node(up, node, cost, 0, false, false);
+                neighbours.Add(nodeUp);
+            }
+
+            if (mMap.IsWalkable(down))
+            {
+                Node nodeUp = new Node(down, node, cost, 0, false, false);
+                neighbours.Add(nodeUp);
+            }
+
+            if (mMap.IsWalkable(left))
+            {
+                Node nodeUp = new Node(left, node, cost, 0, false, false);
+                neighbours.Add(nodeUp);
+            }
+
+            if (mMap.IsWalkable(right))
+            {
+                Node nodeUp = new Node(right, node, cost, 0, false, false);
+                neighbours.Add(nodeUp);
+            }
+            return neighbours;
+        }
+        private void ExpandNode(Node node)
+        {
+            if (mExplored[node.Position.Y, node.Position.X]) return;
+            List<Node> neighbours = CreateNeighbours(node);
+            foreach (var neighbour in neighbours)
+            {
+                if (!(mExplored[node.Position.Y, node.Position.X])) mQueue.Insert(neighbour);
+            }
+
+            mMap.mMap[node.Position.Y, node.Position.X] = node.Cost;
+            mExplored[node.Position.Y, node.Position.X] = true;
+        }
+
+        public void UpdateHeatMap()
+        {
+            var startNode = new Node(mGoalPoint, null, 0, 0, true);
+            // double heuristicValue = startNode.Key;
+            mQueue.Insert(startNode);
+
+            while (!mQueue.IsEmpty())
+            {
+                var heapNode = mQueue.RemoveMin();
+                ExpandNode(heapNode);
+                // Console.Write("I am currently expanding: ");
+                // Console.WriteLine(heapNode.Position);
+            }
+        }
+
+        public void UpdateVectorField()
+        {
+            UpdateHeatMap();
+            mVectorfield.UpdateVectorfield(mMap);
+        }
+
+        public void test1()
+        {
+            Heatmap emptyHeatmap = new Heatmap(8, 8);
+            emptyHeatmap.mMap[2, 1] = Blocked;
+            emptyHeatmap.mMap[3, 1] = Blocked;
+            emptyHeatmap.mMap[4, 1] = Blocked;
+            emptyHeatmap.mMap[4, 2] = Blocked;
+            emptyHeatmap.mMap[2, 3] = Blocked;
+            emptyHeatmap.mMap[3, 3] = Blocked;
+            emptyHeatmap.mMap[4, 3] = Blocked;
+
+            mMap = emptyHeatmap;
+            mVectorfield = new Vectorfield(emptyHeatmap.Width, emptyHeatmap.Height);
+            mQueue = new PriorityQueue();
+            mGoalPoint = new Point(2, 7);
+            UpdateVectorField();
+            string resultHeat = "";
+            for (int i = 0; i < mMap.Height; i++)
+            {
+                for (int j = 0; j < mMap.Width; j++)
+                {
+                    resultHeat += mMap.mMap[i, j].ToString();
+                    resultHeat += "  ";
+                }
+
+                resultHeat += "\n";
+            }
+
+            Console.WriteLine(resultHeat);
+            Console.WriteLine("\n ================================= \n");
+
+            string resultVectors = "";
+            for (int i = 0; i < mMap.Height; i++)
+            {
+                for (int j = 0; j < mMap.Width; j++)
+                {
+                    resultVectors += mVectorfield.Vector(new Point(j, i)).ToString();
+                    resultVectors += "  ";
+                }
+
+                resultVectors += "\n";
+            }
+            Console.WriteLine(resultVectors);
+            Console.WriteLine("\n === \n");
+        }
+    }
+}
---
+++ src/KernelPanic/Heatmap.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace KernelPanic
+{
+    class Heatmap
+    {
+        public const int Blocked = -1;
+
+        public double[,] mMap;
+        private int mWidth;
+        private int mHeight;
+
+        public Heatmap(int width, int height)
+        {
+            mMap = new double[height, width];
+            mWidth = width;
+            mHeight = height;
+        }
+
+        public bool IsWalkable(Point point)
+        {
+            if (point.X >= mWidth || point.Y >= mHeight) return false;
+            if (point.X < 0 || point.Y < 0) return false;
+            return mMap[point.Y, point.X] != Blocked;
+        }
+
+        public Vector2 Gradient(Point point)
+        {
+            Vector2 grad;
+            Point up = new Point(point.X, point.Y - 1);
+            Point down = new Point(point.X, point.Y + 1);
+            Point left = new Point(point.X - 1, point.Y);
+            Point right = new Point(point.X + 1, point.Y);
+            double heatUp = 0;
+            double heatDown = 0;
+            double heatLeft = 0;
+            double heatRight = 0;
+
+            if (!IsWalkable(point)) return new Vector2(Single.NaN, Single.NaN);
+
+            if (IsWalkable(up)) heatUp = mMap[point.Y - 1, point.X];
+            else heatUp = mMap[point.Y, point.X];
+
+            if (IsWalkable(down)) heatDown = mMap[point.Y + 1, point.X];
+            else heatUp = mMap[point.Y, point.X];
+
+            if (IsWalkable(left)) heatLeft = mMap[point.Y, point.X - 1];
+            else heatUp = mMap[point.Y, point.X];
+
+            if (IsWalkable(right)) heatRight = mMap[point.Y, point.X + 1];
+            else heatUp = mMap[point.Y, point.X];
+
+            grad = new Vector2((float)(heatLeft - heatRight), (float)(heatUp - heatDown));
+            return grad;
+        }
+
+        public Vector2 NormalizedGradient(Point point)
+        {
+            Vector2 grad = Gradient(point);
+            if (grad != Vector2.Zero) grad.Normalize();
+            return grad;
+        }
+
+        public int Width { get => mWidth;}
+        public int Height { get => mHeight;}
+    }
+
+    class Vectorfield
+    {
+        private Vector2[,] mVectorField;
+        private int mHeight;
+        private int mWidth;
+        public Vectorfield(int width, int height)
+        {
+            mVectorField = new Vector2[height, width];
+            mHeight = height;
+            mWidth = width;
+        }
+
+        public void UpdateVectorfield(Heatmap map)
+        {
+            if (map.Width > mWidth || map.Height > mHeight) return;
+            for (int i = 0; i < map.Width; i++)
+            {
+                for (int j = 0; j < map.Height; j++)
+                {
+                    mVectorField[j, i] = map.NormalizedGradient(new Point(i, j));
+                }
+            }
+        }
+
+        public Vector2 Vector(Point point)
+        {
+            if (point.X >= mWidth || point.Y >= mHeight) return new Vector2(Single.NaN, Single.NaN);
+            if (point.X < 0 || point.Y < 0) return new Vector2(Single.NaN, Single.NaN);
+            return mVectorField[point.Y, point.X];
+        }
+    }
+}
