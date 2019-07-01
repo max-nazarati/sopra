b5432c065efc70077d24a0d98f250d47edf9d0e8
Zacharias Haeringer <Zacharias.Haeringer@uranus.uni-freiburg.de>
2019-07-01 11:00:31 +0200

    [Cable] uncommenting the correct Konstruktor after content is in pipeline

--- src/KernelPanic/Entities/Cable.cs
+++ src/KernelPanic/Entities/Cable.cs
@@ -4,17 +4,11 @@ namespace KernelPanic.Entities
 {
     internal sealed class Cable : Building
     {
-        
         // todo: correct price
-        // TODO LOAD CABLE INTO CONTENT
-        /* public Cable(SpriteManager spriteManager, SoundManager soundManager) : base(1, spriteManager.CreateCable(), spriteManager)
+        public Cable(SpriteManager spriteManager, SoundManager soundManager) : base(1, spriteManager.CreateCable(),
+            spriteManager)
         {
-            //
-        }
-        */
-
-        public Cable(int price, Sprite sprite, SpriteManager spriteManager) : base(price, sprite, spriteManager)
-        {
-        }
+            
+        } 
     }
 }
