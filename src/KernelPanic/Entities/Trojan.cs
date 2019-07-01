89ec074e1bd4dd4557d6301fa0709956c6cf0d9a
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-02 01:49:04 +0200

    [Trojan] Don't crash when the trojan was spawned without a wave.

--- src/KernelPanic/Entities/Trojan.cs
+++ src/KernelPanic/Entities/Trojan.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 
 namespace KernelPanic.Entities
 {
@@ -14,8 +15,15 @@ namespace KernelPanic.Entities
 
         protected override void DidDie()
         {
-            for (var i = 0; i < (SpawnsDouble ? 10 : 5); ++i)
-                Wave.SpawnChild(new Bug(SpriteManager) {Sprite = {Position = Sprite.Position}});
+            if (Wave.IsValid)
+            {
+                for (var i = 0; i < (SpawnsDouble ? 10 : 5); ++i)
+                    Wave.SpawnChild(new Bug(SpriteManager) {Sprite = {Position = Sprite.Position}});
+            }
+            else
+            {
+                Console.WriteLine("I would like to make some bugs, but my wave is not set.");
+            }
 
             base.DidDie();
         }
--- src/KernelPanic/Waves/WaveReference.cs
+++ src/KernelPanic/Waves/WaveReference.cs
@@ -8,6 +8,8 @@ namespace KernelPanic.Waves
         internal int Index { get; }
         internal Action<Unit> SpawnChild { get; }
 
+        internal bool IsValid => Index > 0;
+
         internal WaveReference(int index, Action<Unit> spawnChild)
         {
             Index = index;
