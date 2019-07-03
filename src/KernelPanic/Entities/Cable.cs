4efd48c413b2ef10785898933acf40d21c0a53fa
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-03 13:32:43 +0200

    [Cable] Better warning suppression

--- src/KernelPanic/Entities/Cable.cs
+++ src/KernelPanic/Entities/Cable.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace KernelPanic.Entities
 {
     internal sealed class Cable : Building
     {
-        // ReSharper disable once UnusedParameter.Local – the parameters are required for Building.Create to work!
+        // The parameters are required for Building.Create to work!
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
         public Cable(SpriteManager spriteManager, SoundManager soundManager)
             : base(10, spriteManager.CreateCable(), spriteManager)
         {
