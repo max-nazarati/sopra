598be935b4c5380c01ad171f5b18a1d923b5377a
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-07 16:41:21 +0200

    [Building] Tint it according to its state

--- src/KernelPanic/Entities/Building.cs
+++ src/KernelPanic/Entities/Building.cs
@@ -58,29 +58,32 @@ namespace KernelPanic.Entities
 
         private int BitcoinWorth { get; set; }
 
-        internal State StateProperty { get; set; }
+        private BuildingState mBuildingState;
 
-        internal enum State
+        internal BuildingState State
         {
-            /// <summary>
-            /// The building is able to act, that means it is able to attack enemies.
-            /// </summary>
-            Active,
-            
-            /// <summary>
-            /// The building has been bought and is waiting to become active, that is when no enemies are at its position.
-            /// </summary>
-            Inactive,
-            
-            /// <summary>
-            /// Used during selection of a new place for a building when the current position is not allowed.
-            /// </summary>
-            Invalid,
-            
-            /// <summary>
-            /// Used during selection of a new place for a building when the current position is allowed.
-            /// </summary>
-            Valid
+            get => mBuildingState;
+            set
+            {
+                mBuildingState = value;
+                switch (value)
+                {
+                    case BuildingState.Active:
+                        Sprite.TintColor = Color.White;
+                        break;
+                    case BuildingState.Inactive:
+                        Sprite.TintColor = Color.Gray;
+                        break;
+                    case BuildingState.Invalid:
+                        Sprite.TintColor = Color.Red;
+                        break;
+                    case BuildingState.Valid:
+                        Sprite.TintColor = Color.Green;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(value), value, null);
+                }
+            }
         }
 
         #region Actions
@@ -123,5 +126,28 @@ namespace KernelPanic.Entities
         }
 
         #endregion
+    }
+
+    internal enum BuildingState
+    {
+        /// <summary>
+        /// The building is able to act, that means it is able to attack enemies.
+        /// </summary>
+        Active,
+
+        /// <summary>
+        /// The building has been bought and is waiting to become active, that is when no enemies are at its position.
+        /// </summary>
+        Inactive,
+
+        /// <summary>
+        /// Used during selection of a new place for a building when the current position is not allowed.
+        /// </summary>
+        Invalid,
+
+        /// <summary>
+        /// Used during selection of a new place for a building when the current position is allowed.
+        /// </summary>
+        Valid
     }
 }
