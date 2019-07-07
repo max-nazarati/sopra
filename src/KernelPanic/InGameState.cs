59dd119fc1b27ca008d73d7dd403cb09414f3da1
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-07 23:36:25 +0200

    [InGameState] Run the building buyer before the selection manager
    This way we can place buildings without having to worry about entities
    below the mouse.

--- src/KernelPanic/InGameState.cs
+++ src/KernelPanic/InGameState.cs
@@ -57,8 +57,8 @@ namespace KernelPanic
             // Update the overall play time.
             GameStateManager.Statistics.Update(gameTime);
 
-            mSelectionManager.Update(inputManager);
             mBuildingBuyer.Update(inputManager);
+            mSelectionManager.Update(inputManager);
 
             mBoard.Update(gameTime, inputManager);
             var gameState = mBoard.CheckGameState();
