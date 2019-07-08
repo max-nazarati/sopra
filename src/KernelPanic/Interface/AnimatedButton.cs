4be563dc2218367c2e9d69e4a3eeef0349d15620
Johannes <johannesmannhardt@web.de>
2019-07-08 19:09:23 +0200

    Fixed file directory

--- src/KernelPanic/Interface/AnimatedButton.cs
+++ src/KernelPanic/Interface/AnimatedButton.cs
@@ -1,33 +1,32 @@
-using System;
 using KernelPanic.Entities.Units;
-using KernelPanic.Input;
-using KernelPanic.Sprites;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-
-namespace KernelPanic.Interface
-{
-    internal class AnimatedButton : TextButton
-    {
-        private readonly ImageSprite ButtonOverlay;
-        private readonly Hero mHero;
-        internal AnimatedButton(SpriteManager sprites, Hero hero, int width = 250, int height = 70) : base(sprites, width, height)
-        {
-            mHero = hero;
-            ButtonOverlay = sprites.CreateColoredRectangle(1, 1, new Color[]{new Color(0.8f, 0.8f, 0.8f, 0.5f)});
-        }
-        
-        public override void Update(InputManager inputManager, GameTime gameTime)
-        {
-            base.Update(inputManager, gameTime);
-            var width = (int) (50 * mHero.RemainingCooldownTime);
-            ButtonOverlay.DestinationRectangle = new Rectangle((int)Position.X, (int)Position.Y, width, 70);
-        }
-        
-        public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
-        {
-            base.Draw(spriteBatch, gameTime);
-            ButtonOverlay.Draw(spriteBatch, gameTime);
-        }
-    }
-}
+ using KernelPanic.Input;
+ using KernelPanic.Sprites;
+ using Microsoft.Xna.Framework;
+ using Microsoft.Xna.Framework.Graphics;
+ 
+ namespace KernelPanic.Interface
+ {
+     internal class AnimatedButton : TextButton
+     {
+         private readonly ImageSprite mButtonOverlay;
+         private readonly Hero mHero;
+         internal AnimatedButton(SpriteManager sprites, Hero hero, int width = 250, int height = 70) : base(sprites, width, height)
+         {
+             mHero = hero;
+             mButtonOverlay = sprites.CreateColoredRectangle(1, 1, new Color[]{new Color(0.8f, 0.8f, 0.8f, 0.5f)});
+         }
+         
+         public override void Update(InputManager inputManager, GameTime gameTime)
+         {
+             base.Update(inputManager, gameTime);
+             var width = (int) (50 * mHero.RemainingCooldownTime);
+             mButtonOverlay.DestinationRectangle = new Rectangle((int)Position.X, (int)Position.Y, width, 70);
+         }
+         
+         public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+         {
+             base.Draw(spriteBatch, gameTime);
+             mButtonOverlay.Draw(spriteBatch, gameTime);
+         }
+     }
+ }
