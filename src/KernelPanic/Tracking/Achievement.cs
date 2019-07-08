a6a0bea9365881960d7b93e8d000e708c009ec15
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-08 17:09:51 +0200

    [Achievements] First steps

--- src/KernelPanic/Tracking/Achievement.cs
+++ src/KernelPanic/Tracking/Achievement.cs
@@ -7,6 +7,13 @@ namespace KernelPanic.Tracking
 {
     internal enum Achievement
     {
+        Win1,
+        Win10,
+        Win100,
+
+        Lose1,
+        Lose10,
+        Lose100
     }
 
     internal static class Achievements
@@ -16,6 +23,20 @@ namespace KernelPanic.Tracking
         {
             switch (achievement)
             {
+                case Achievement.Win1:
+                    return "First Victory!";
+                case Achievement.Win10:
+                    return "GG EASY";
+                case Achievement.Win100:
+                    return "Is Dis Tetris?";
+
+                case Achievement.Lose1:
+                    return "Unlucky Loss";
+                case Achievement.Lose10:
+                    return "Rekt";
+                case Achievement.Lose100:
+                    return "Complete Humiliation";
+
                 default:
                     throw new InvalidEnumArgumentException(nameof(achievement), (int) achievement, typeof(Achievement));
             }
@@ -24,6 +45,40 @@ namespace KernelPanic.Tracking
         internal static string Description(this  Achievement achievement)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    internal abstract class AchievementProgress
+    {
+        internal static AchievementProgress Track(Achievement achievement)
+        {
+            switch (achievement)
+            {
+                case Achievement.Win1:
+                    return new SingleEventCountProgress(Event.Id.GameWon, 1);
+                case Achievement.Win10:
+                    return new SingleEventCountProgress(Event.Id.GameWon, 10);
+                case Achievement.Win100:
+                    return new SingleEventCountProgress(Event.Id.GameWon, 100);
+                case Achievement.Lose1:
+                    return new SingleEventCountProgress(Event.Id.GameLost, 1);
+                case Achievement.Lose10:
+                    return new SingleEventCountProgress(Event.Id.GameLost, 10);
+                case Achievement.Lose100:
+                    return new SingleEventCountProgress(Event.Id.GameLost, 100);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(achievement), achievement, null);
+            }
+        }
+    }
+
+    internal sealed class SingleEventCountProgress : AchievementProgress
+    {
+        private int mRemaining;
+
+        internal SingleEventCountProgress(Event.Id eventId, int count)
+        {
+            mRemaining = count;
         }
     }
 }
