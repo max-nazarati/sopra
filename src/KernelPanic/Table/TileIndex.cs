37469403356e4948dd96deb282722ca724c8de51
Janek Spaderna <janek.spaderna@pluto.uni-freiburg.de>
2019-07-11 12:50:03 +0200

    [TileIndex] Function to rescale an index

--- src/KernelPanic/Table/TileIndex.cs
+++ src/KernelPanic/Table/TileIndex.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
 namespace KernelPanic.Table
@@ -22,6 +23,25 @@ namespace KernelPanic.Table
             Row = point.Y;
             Column = point.X;
             SubTileCount = subTileCount;
+        }
+
+        /// <summary>
+        /// Turns a <see cref="TileIndex"/> into a set of indices which cover the same area. This function only works
+        /// correctly, if <paramref name="subTileCount"/> is a multiple <see cref="SubTileCount"/> and
+        /// <paramref name="subTileCount"/> is greater than or equal to <see cref="SubTileCount"/>;
+        /// </summary>
+        /// <param name="subTileCount">The wanted <see cref="SubTileCount"/>.</param>
+        /// <returns>A set of indices covering the same area.</returns>
+        internal IEnumerable<TileIndex> Rescaled(int subTileCount)
+        {
+            var mul = subTileCount / SubTileCount;
+            for (int row = Row * mul, i = 0; i < subTileCount; i++, row++)
+            {
+                for (int col = Column * mul, j = 0; j < subTileCount; j++, col++)
+                {
+                    yield return new TileIndex(row, col, subTileCount);
+                }
+            }
         }
 
         public override string ToString() => $"[row: {Row}, col: {Column}, sub-tiles: {SubTileCount}]";
